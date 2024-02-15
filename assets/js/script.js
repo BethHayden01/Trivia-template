a398f1d8cdfa523908f184346b48e8f56ea9a964
BethHayden01 <bethanyhayden12345@outlook.com>
2024-02-15 19:44:12 +0000

    Fix bug in show answers element

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,13 +57,11 @@ function showQuestion(){
     questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
 
     currentQuestion.answers.forEach(answer => {
-        const button = document.createElement()
+        const button = document.createElement("button")
         button.innerHTML = answer.text;
         button.classList.add("btn");
         answerButton.appendChild(button);
-
-    });
-        
+    });      
 }
 
 startQuiz();
